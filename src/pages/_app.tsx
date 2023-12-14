ec217ccf8b28f0a49551119a9de17744f0cb59f4
Jaydeep Patel <jaydeep.mobilefirst@gmail.com>
2023-12-14 18:50:37 +0530

    boiler

---
+++ src/pages/_app.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import {
+  DynamicContextProvider,
+  useWalletItemActions,
+  useDynamicContext,
+  useSendBalance,
+} from "@dynamic-labs/sdk-react";
+import { useEffect, useState } from "react";
+import "../styles/globals.css";
+import { ethers } from "ethers";
+
+const configuration = {
+  environmentId: "c2c8cbaa-1aa8-4030-b258-1402ad88aa75",
+  shadowDOMEnabled: false,
+  newToWeb3WalletChainMap: {
+    primary_chain: "evm",
+    wallets: {
+      evm: "metamask",
+    },
+  },
+};
+
+const ConnectWalletButtons = () => {
+  const { openWallet } = useWalletItemActions();
+
+  const { handleLogOut } = useDynamicContext();
+
+  return (
+    <div
+      className="flex justify-center items-center"
+      style={{
+        display: "flex",
+        margin: "20px",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "20px",
+      }}
+    >
+      <button
+        onClick={() => openWallet("metamask")}
+        className="bg-blue-600 text-white h-8 px-4 rounded-md"
+      >
+        Connect Metamask
+      </button>
+      <button
+        onClick={() => openWallet("coinbase")}
+        className="bg-blue-600 text-white h-8 px-4 rounded-md"
+      >
+        Connect Coinbase
+      </button>
+      <button
+        onClick={() => openWallet("walletconnect")}
+        className="bg-blue-600 text-white h-8 px-4 rounded-md"
+      >
+        Connect WalletConnect
+      </button>
+
+      <button
+        onClick={handleLogOut}
+        className="bg-red-600 text-white h-8 px-4 rounded-md"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+const MyComponent = () => {
+  const { primaryWallet, rpcProviders, connectedWallets } = useDynamicContext();
+  const { open } = useSendBalance();
+  const [balance, setBalance] = useState<any>(null);
+
+  const sendTransaction = async () => {
+    if (primaryWallet) {
+      const signer: any = await primaryWallet.connector.getSigner();
+      const transaction = {
+        to: "0xFC162195D5425b4F5363F1E047fd02EC2dcDED47",
+        value: ethers.utils.parseEther("0.0001"),
+        // data: "0x0",
+      };
+      const txHash = await signer.sendTransaction(transaction);
+      return txHash;
+    }
+    return null;
+  };
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      if (primaryWallet && rpcProviders.evmDefaultProvider?.provider) {
+        const provider = rpcProviders.evmDefaultProvider.provider;
+        const value = await provider.getBalance(primaryWallet.address);
+        setBalance(value.toString());
+      }
+    };
+    fetchBalance();
+  }, [primaryWallet, rpcProviders]);
+
+  console.log("connectedWallets", connectedWallets);
+  console.log("primaryWallet", primaryWallet);
+  console.log("balance", balance);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "20px",
+        justifyContent: "center",
+        alignItems: "start",
+        gap: "20px",
+      }}
+    >
+      <p>Address: {primaryWallet?.address}</p>
+      <p>Balance: {balance}</p>
+      <button
+        onClick={() => sendTransaction()}
+        className="bg-green-600 text-white h-8 px-4 rounded-md"
+      >
+        Send Transaction
+      </button>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <DynamicContextProvider settings={configuration}>
+      <ConnectWalletButtons />
+      <MyComponent />
+    </DynamicContextProvider>
+  );
+}
+
+export default App;
